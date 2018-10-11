b614ced364cb3e98fb9672bce262c515e37be585
Alexandre Teixeira <alexanddy99@gmail.com>
2018-10-11 12:26:01 -0300

    correção gitignore

--- functions/lib/util/Constantes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Constantes {
-}
-Constantes.END_POINT_NOTES = "/notes";
-exports.Constantes = Constantes;
-//# sourceMappingURL=Constantes.js.map
