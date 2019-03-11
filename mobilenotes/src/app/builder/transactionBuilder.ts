e07078ff81825b50d89cbf7168afa411a1209e43
Alexandre Teixeira <alexanddy99@gmail.com>
2019-03-11 15:59:30 -0300

    implementação do builder de transaction

---
+++ mobilenotes/src/app/builder/transactionBuilder.ts
@@ -0,0 +1,36 @@
+import { TypeTransaction } from './../model/typeTransaction';
+import { NumberUtils } from '../util/numberUtils';
+import { Transaction } from '../model/transaction';
+export class TransactionBuilder {
+
+    private value: number;
+    private typeTransaction: TypeTransaction;
+    private date: Date;
+    private category: string = 'indefinida';
+
+    constructor() { }
+
+    addValue(value: string): TransactionBuilder {
+        this.value = NumberUtils.parseNumber(value);
+        return this;
+    }
+
+    addTypeTransaction(typeTransaction: string): TransactionBuilder {
+        this.typeTransaction = TypeTransaction[typeTransaction];
+        return this;
+    }
+
+    addDate(date: string): TransactionBuilder {
+        this.date = new Date(date);
+        return this;
+    }
+
+    addCategory(category: string): TransactionBuilder {
+        this.category = category;
+        return this;
+    }
+
+    build(): Transaction {
+        return new Transaction(this.value, this.typeTransaction, this.date, this.category);
+    }
+}
